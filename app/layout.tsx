787105f4c7d2cb986f9691b45d3397bc47296499
Jakob Pless <pless84@gmail.com>
2025-09-19 13:11:03 +0200

    Analytics and insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,11 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="en">
-			<body className={`${inter.className} antialiased`}>{children}</body>
+			<body className={`${inter.className} antialiased`}>
+				{children}
+				<SpeedInsights />
+				<Analytics />
+			</body>
 		</html>
 	);
 }
